032803bb33d8ac042dfee037ed2ea4079c5b307a
jrhineh5 <61155638+jrhineh5@users.noreply.github.com>
2021-03-16 23:23:30 -0400

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var widget = document.createElement('iframe');
-    widget.src = "//popup/html";
-    widget.id = 'widget';
-    document.body.appendChild(widget);
-
-    //assign your widget variables
-})();
